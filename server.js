a5c296530b6d88904482d39aa472c9353d26e4df
Pavan <reddypavankumar709@gmail.com>
2026-02-23 10:30:39 +0530

    refactoring code

--- server.js
+++ server.js
@@ -8,21 +8,19 @@ import cors from 'cors'
 const app = express()
 
 
-// Database connection
 await connectDB()
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
 
+// Root route (optional but recommended)
+app.get("/", (req, res) => {
+  res.json({ message: "Backend Running 🚀" })
+})
+
 // Routes
 app.use("/api/admin", adminRouter)
 app.use("/api/blog", blogRouter)
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
 
 export default app
