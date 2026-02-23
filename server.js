50d5e7b6e8bd124278cfbd702e732b9010e736f1
Pavan <reddypavankumar709@gmail.com>
2026-02-23 10:57:56 +0530

    Refactoring code

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.use(express.json())
 app.use("/api/admin", adminRouter)
 app.use("/api/blog", blogRouter)
 
-const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
 
 export default app
